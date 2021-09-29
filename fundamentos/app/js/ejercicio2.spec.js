15ed19cc0a0a35a31198848b6af7e14a495de2d4
f12mori <kikomommmlo@hotmail.com>
2021-09-29 13:23:23 +0200

    mig

---
+++ fundamentos/app/js/ejercicio2.spec.js
@@ -0,0 +1,11 @@
+fdescribe('Pruebas de los ejercicios', function(){
+    describe('Ejercicio 3', function(){
+        it('Longitud 4 datos 3', function(){
+            
+            let resultado=ejercicio2(50);
+
+        })
+    })
+
+
+})
