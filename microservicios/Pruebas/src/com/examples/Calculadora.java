103c4e7679a87bb7f58fa3a7af0880083259532b
f12mori <kikomommmlo@hotmail.com>
2021-10-18 16:52:03 +0200

    microservivios

---
+++ microservicios/Pruebas/src/com/examples/Calculadora.java
@@ -0,0 +1,20 @@
+package com.examples;
+
+public class Calculadora {
+
+	public double suma(double a, double b) {
+		return a + b;
+	}
+	public double resta(double a, double b) {
+		return a - b;
+	}
+	public double multiplica(double a, double b) {
+		return a * b;
+	}
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
