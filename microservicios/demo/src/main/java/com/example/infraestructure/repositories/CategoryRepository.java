fc4c6a8d0d6cb1c2fa27e4ff8bce1f362f514737
f12mori <kikomommmlo@hotmail.com>
2021-10-25 12:22:06 +0200

    .

--- microservicios/demo/src/main/java/com/example/infraestructure/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.infraestructure.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.domains.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
