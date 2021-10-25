741607dc521d9311cb23feb2f41410657dcf24ba
f12mori <kikomommmlo@hotmail.com>
2021-10-25 12:21:36 +0200

    refactorizar demos

--- microservicios/demo/src/main/java/com/example/application/resource/ActorResource.java
+++ microservicios/demo/src/main/java/com/example/application/resource/ActorResource.java
@@ -1,45 +1,28 @@
 package com.example.application.resource;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.transaction.Transactional;
-import javax.validation.ConstraintViolation;
 import javax.validation.Valid;
-import javax.validation.Validator;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.domains.contracts.services.ActorService;
-import com.example.domains.entities.FilmActor;
+import com.example.domains.entities.Actor;
 import com.example.domains.entities.dtos.ActorDTO;
-import com.example.domains.entities.dtos.FilmShort;
-import com.example.exceptions.BadRequestException;
-import com.example.exceptions.DuplicateKeyException;
-import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
-import org.springframework.http.HttpStatus;
+import antlr.collections.List;
+
 
 @RestController
 @RequestMapping(path = "/actores")
@@ -48,63 +31,28 @@ public class ActorResource {
 	ActorService srv;
 	
 	@GetMapping
-	public List<ActorDTO> getAll(@RequestParam(required = false) String sort) {
-		if(sort== null)
-			return srv.getByProjection(ActorDTO.class);
-		else
-			return (List<ActorDTO>) srv.getByProjection(Sort.by(sort), ActorDTO.class);
+	public List<ActorDTO> getall(){
+		return srv.getByProjection(ActorDTO.class);
 	}
 	
-	@GetMapping(params = "page")
-	public Page<ActorDTO> getAllPageable(Pageable item) {
-		return srv.getByProjection(item, ActorDTO.class);
-	}
-
-	@GetMapping(path = "/{id}")
-	public ActorDTO getOne(@PathVariable int id) throws NotFoundException {
-		var actor = srv.getOne(id);
-		if(actor.isEmpty())
-			throw new NotFoundException();
-		else
-			return ActorDTO.from(actor.get());
-	}
 	
-	@GetMapping(path = "/{id}/peliculas")
-	@Transactional
-	public List<FilmShort> getPelis(@PathVariable int id) throws NotFoundException {
+	@GetMapping(path="/id")
+	public ActorDTO getOne(@PathVariable int id) throws NotFoundException{
 		var actor = srv.getOne(id);
 		if(actor.isEmpty())
 			throw new NotFoundException();
 		else {
-			return (List<FilmShort>) actor.get().getFilmActors().stream().map(item -> FilmShort.from(item)).collect(Collectors.toList());
+			return ActorDTO.from(ActorDTO.get());
 		}
+		
 	}
 	
+	
 	@PostMapping
-	public ResponseEntity<Object> create(@Valid @RequestBody ActorDTO item) throws BadRequestException, DuplicateKeyException, InvalidDataException {
-		if(item == null)
-			throw new BadRequestException("Faltan los datos");
-		var newItem = srv.add(ActorDTO.from(item));
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-			.buildAndExpand(newItem.getActorId()).toUri();
-		return ResponseEntity.created(location).build();
-
+	public ResponseEntity<Object> create(@Valid @RequestBody ActorDTO item){
+	if(item== null)
+		throw new BadRequestException("Faltan datos");
+	
+		
 	}
-
-	@PutMapping("/{id}")
-	//@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ActorDTO update(@PathVariable int id, @Valid @RequestBody ActorDTO item) throws BadRequestException, NotFoundException, InvalidDataException {
-		if(item == null)
-			throw new BadRequestException("Faltan los datos");
-		if(id != item.getActorId())
-			throw new BadRequestException("No coinciden los identificadores");
-		return ActorDTO.from(srv.modify(ActorDTO.from(item)));	
-	}
-
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable int id) {
-		srv.deleteById(id);
-	}
-
 }
